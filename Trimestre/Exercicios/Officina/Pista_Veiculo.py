cf332137208d9441e8fdbe60577e9fe21cf7f729
Daniel Gustavo de Lucca <daniel.gusdelucca@gmail.com>
2026-04-15 12:11:53 +0000

    Add Pista_Veicuilo

--- Trimestre/Exercicios/Officina/Pista_Veiculo.py
+++ Trimestre/Exercicios/Officina/Pista_Veiculo.py
@@ -1,1 +1,27 @@
-class
+class Veiculo():
+    def __init__(self, placa, cor, tanque: int, odometro: float, rendimento: float, desenho):
+        self.placa = placa
+        self.cor = cor
+        self.tanque = tanque
+        self.odometro = odometro
+        self.rendimento = rendimento
+        self.desenho = desenho
+
+    def __str__(self):
+        return f"Placa: {self.placa}, Cor: {self.cor}, Tanque: {self.tanque}, Odometro: {self.odometro}, rendimento: {self.rendimento}, desenho: {self.desenho}"
+    
+    def andar(self):
+    #diminuir 1 do tanque
+        self.tanque = self.tanque-1
+    #acrescentar no odometro a kilometragem percorrida com 1 litro(rendimento)
+        self.odometro = self.odometro+self.rendimento
+
+class Pista:
+    def __init__(self, extensao: int, veiculos):
+        self.extensao = extensao
+        self.veiculos = []
+    
+    def __str__(self):
+        return f"Extensão: {self.extensao}, Veiculos: {self.veiculos}"
+
+        
